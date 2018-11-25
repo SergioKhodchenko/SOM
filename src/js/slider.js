ea0b44ce71bb17191d6ed9c82f36e1959cd3fc95
Sergio <sirogxod@gmail.com>
2018-11-25 17:55:48 +0200

    final 25.11

--- src/js/slider.js
+++ src/js/slider.js
@@ -20,3 +20,43 @@ const sliderScrollHandle = () => {
 	}
 }
 window.addEventListener("scroll", sliderScrollHandle);
+
+
+//-----slider animation-------
+const leftImages = document.querySelectorAll('.left-image');
+const leftImagesArray = Array.from(leftImages);
+const rightImages = document.querySelectorAll('.right-image');
+const rightImagesArray = Array.from(rightImages);
+
+let imageIndexL = 0;
+let imageIndexR = 0;
+
+function slideAllImage() {
+        slideLeftImage();
+        slideRightImage();
+}
+function slideLeftImage() {
+        leftImagesArray[leftImagesArray.length - 1].classList.remove('slided');
+        if (imageIndexL === leftImagesArray.length) {
+                imageIndexL = 0;
+        }
+        leftImagesArray[imageIndexL].classList.add('slided'); 
+        if (imageIndexL >= 1) {
+                leftImagesArray[imageIndexL - 1].classList.remove('slided'); 
+        }
+        imageIndexL +=1;
+}
+
+function slideRightImage() {
+        rightImagesArray[rightImagesArray.length - 1].classList.remove('slided');
+        if (imageIndexR === rightImagesArray.length) {
+                imageIndexR = 0;
+        }
+        rightImagesArray[imageIndexR].classList.add('slided'); 
+        if (imageIndexR >= 1) {
+                rightImagesArray[imageIndexR - 1].classList.remove('slided'); 
+        }
+        imageIndexR +=1;
+}
+
+const slider = setInterval(slideAllImage, 1800);
